86c84522c2695a7df8ecab608d374aaf73717c2d
Arturo Cano Mercado <borpro@outlook.com>
2022-04-29 23:46:16 -0500

    Avances

---
+++ AdminHotelApi/Controllers/Api/ClientesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AdminHotelApi.Data;
+using AdminHotelApi.Models;
+
+namespace AdminHotelApi.Controllers.Api
+{
+    public class ClientesController : ApiController
+    {
+        private AdminHotelApiContext db = new AdminHotelApiContext();
+
+        // GET: api/Clientes
+        public IQueryable<Cliente> GetClientes()
+        {
+            return db.Clientes;
+        }
+
+        // GET: api/Clientes/5
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult GetCliente(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // PUT: api/Clientes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCliente(int id, Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cliente.HotelId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Clientes
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult PostCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Clientes.Add(cliente);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClienteExists(cliente.HotelId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = cliente.HotelId }, cliente);
+        }
+
+        // DELETE: api/Clientes/5
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult DeleteCliente(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+
+            return Ok(cliente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return db.Clientes.Count(e => e.HotelId == id) > 0;
+        }
+    }
+}
---
+++ AdminHotelApi/Controllers/Api/HotelesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AdminHotelApi.Data;
+using AdminHotelApi.Models;
+
+namespace AdminHotelApi.Controllers.Api
+{
+    public class HotelesController : ApiController
+    {
+        private AdminHotelApiContext db = new AdminHotelApiContext();
+
+        // GET: api/Hoteles
+        public IQueryable<Hotel> GetHoteles()
+        {
+            return db.Hoteles;
+        }
+
+        // GET: api/Hoteles/5
+        [ResponseType(typeof(Hotel))]
+        public IHttpActionResult GetHotel(int id)
+        {
+            Hotel hotel = db.Hoteles.Find(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hotel);
+        }
+
+        // PUT: api/Hoteles/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutHotel(int id, Hotel hotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hotel.HotelId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(hotel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Hoteles
+        [ResponseType(typeof(Hotel))]
+        public IHttpActionResult PostHotel(Hotel hotel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Hoteles.Add(hotel);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = hotel.HotelId }, hotel);
+        }
+
+        // DELETE: api/Hoteles/5
+        [ResponseType(typeof(Hotel))]
+        public IHttpActionResult DeleteHotel(int id)
+        {
+            Hotel hotel = db.Hoteles.Find(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            db.Hoteles.Remove(hotel);
+            db.SaveChanges();
+
+            return Ok(hotel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HotelExists(int id)
+        {
+            return db.Hoteles.Count(e => e.HotelId == id) > 0;
+        }
+    }
+}
