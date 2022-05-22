582dc020354121f4111d708de6955781db422f86
Arturo Cano Mercado <borpro@outlook.com>
2022-05-22 01:08:32 -0500

    Implementacion de log4net

--- AdminHotelApi/Models/CustomExceptionFilter.cs
+++ AdminHotelApi/Models/CustomExceptionFilter.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Web;
 using System.Web.Http.Filters;
 
@@ -10,28 +12,31 @@ namespace AdminHotelApi.Models
 {
     public class CustomExceptionFilter: ExceptionFilterAttribute
     {
+        private static ILog Log;
+
         public override void OnException(HttpActionExecutedContext actionExecutedContext)
         {
             base.OnException(actionExecutedContext);
 
-
-            string exceptionMessage = string.Empty;
+            string exceptionMessage;
+            MethodBase method;
             if (actionExecutedContext.Exception.InnerException == null)
             {
-                exceptionMessage = actionExecutedContext.Exception.Message;
+                exceptionMessage = actionExecutedContext.Exception.ToString();
+                method = actionExecutedContext.Exception.TargetSite;
             }
             else
             {
-                exceptionMessage = actionExecutedContext.Exception.InnerException.Message;
+                exceptionMessage = actionExecutedContext.Exception.InnerException.ToString();
+                method = actionExecutedContext.Exception.InnerException.TargetSite;
             }
-
-
+            Log = LogManager.GetLogger(method.DeclaringType);
+            Log.Error(exceptionMessage);
 
             //We can log this exception message to the file or database.
             var response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
             {
-                Content = new StringContent("An unhandled exception was thrown by service."),
-                    ReasonPhrase = "Internal Server Error.Please Contact your Administrator."
+                Content = new StringContent("Internal Server Error.Please Contact your Administrator.")
             };
             actionExecutedContext.Response = response;
         }
