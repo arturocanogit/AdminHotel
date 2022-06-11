c7302a242e82b1d2f697fb217ced45309753e13c
arturocanogit <borpro@outlook.com>
2022-06-11 17:21:12 -0500

    ajustes

--- AdminHotelApi/Models/Dtos/ArchivoDto.cs
+++ AdminHotelApi/Models/Dtos/ArchivoDto.cs
@@ -9,7 +9,7 @@ namespace AdminHotelApi.Models.Dtos
     public class ArchivoDto : IArchivo
     {
         public string Nombre { get; set; }
-        public int Tamanio { get { return Contenido.Length; } }
+        public int Tamanio { get { return Contenido != null ? Contenido.Length : 0; } }
         public byte[] Contenido { get; set; }
         public ArchivoDto()
         {
